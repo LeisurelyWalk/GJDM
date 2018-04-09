084c4a06150cf3bf7d62d9f8be5caaa825e23f37
leisurelyWalk <kzhouxd@163.com>
2018-04-09 10:59:53 +0800

    GJDM

--- SRmain.m
+++ SRmain.m
@@ -1,21 +1,21 @@
 img_path = 'testTD';
 type = '*.tiff';
 time=zeros(1,3);
-img_dir = dir(fullfile(img_path, type));%?·å???»¶??bmp
+img_dir = dir(fullfile(img_path, type));
 
-img_num = length(img_dir);%?¾ç?ä¸??
+img_num = length(img_dir);
 % nper_img = zeros(1, img_num);
-TD = 2;
+TD = 3;
 for ii = 1:length(img_dir),
     imname = img_dir(ii).name;
     my = Demo_mySR(time,img_path,TD,imname);
 %     im = imread(fullfile(img_path, img_dir(ii).name));
-%     nper_img(ii) = prod(size(im));%è®¡ç??°ç???????ä¹?§¯
+%     nper_img(ii) = prod(size(im));
 end
 TD = 2;
 for ii = 1:length(img_dir),
     imname = img_dir(ii).name;
     my = Demo_mySR(time,img_path,TD,imname);
 %     im = imread(fullfile(img_path, img_dir(ii).name));
-%     nper_img(ii) = prod(size(im));%è®¡ç??°ç???????ä¹?§¯
+%     nper_img(ii) = prod(size(im));
 end
